31124691b5d94a5652692a613afacd941f847251
santhoshnagireddy-ojas <santhoshreddy1053@gmail.com>
2022-03-26 11:12:51 +0530

    hi

--- SpringRestappPostManWithEntityClass/src/main/java/com/example/demo/beans/Registration.java
+++ SpringRestappPostManWithEntityClass/src/main/java/com/example/demo/beans/Registration.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//santu
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
