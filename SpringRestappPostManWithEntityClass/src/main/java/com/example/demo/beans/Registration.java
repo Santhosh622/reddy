ad91be1e4f085a864e99fda1211ed59bc2bdffd9
NagireddySantu <santhoshreddy1053@gmail.com>
2022-11-04 18:12:42 +0530

    ok

--- SpringRestappPostManWithEntityClass/src/main/java/com/example/demo/beans/Registration.java
+++ SpringRestappPostManWithEntityClass/src/main/java/com/example/demo/beans/Registration.java
@@ -1,6 +1,7 @@
 package com.example.demo.beans;
 /////
 /////Non-Prod
+////
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- SpringRestappPostManWithEntityClass/src/main/java/com/example/demo/beans/RegistrationModel.java
+++ SpringRestappPostManWithEntityClass/src/main/java/com/example/demo/beans/RegistrationModel.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /////////Non_Prod
+/////
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
