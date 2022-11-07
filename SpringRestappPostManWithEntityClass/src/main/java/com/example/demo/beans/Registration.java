f39d002c93b6a05eb5dc07ab3be201f5836c86c1
Santhosh622 <84134338+Santhosh622@users.noreply.github.com>
2022-11-07 18:09:19 +0530

    Update Registration.java

--- SpringRestappPostManWithEntityClass/src/main/java/com/example/demo/beans/Registration.java
+++ SpringRestappPostManWithEntityClass/src/main/java/com/example/demo/beans/Registration.java
@@ -15,7 +15,11 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+//
+//
+//
+//
+//
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
